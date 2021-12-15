9fbd498b6689c238b4434b75ac3c993641166db6
kenil333 <kenil.jasani.aitce@gmail.com>
2021-12-15 09:20:54 +0530

    this_is_first_from_Mac

--- lib/view/screens/dashboard/bottom_bar.dart
+++ lib/view/screens/dashboard/bottom_bar.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
